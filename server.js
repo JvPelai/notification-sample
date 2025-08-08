6f5df4015fde474e944b4dae084fdbbd80fca92b
JvPelai <jv.pelai@gmail.com>
2025-08-08 17:17:10 -0300

    socket.io server setup

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
+io.on('connection', (socket) => {
+    console.log('New client connected:', socket.id);
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+    socket.on('subscribe', (userId) => {
+        socket.join(userId);
+        console.log(`User ${userId} subscribed`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected:', socket.id);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
